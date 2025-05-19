2c5f44e9bbf4c9cbb935edd23cb2e470b4087f86
ttehasi <tatiana.chudowa@my.com>
2025-05-19 13:50:14 +0300

    add README.md

--- Makefile
+++ Makefile
@@ -12,14 +12,13 @@ lint:
 	uv run ruff check 
 
 render-start:
+	python3 -m page_analyzer.db.database
+	gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
+build: install build-db
+
+build-db:
 	uv run python3 -m page_analyzer.db.database
-	uv run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
-
-build:
-	./build.sh
-
-# build-db:
-# 	uv run python3 -m page_analyzer.db.database
 
 prod:
 	docker run -it -e 'DATABASE_URL=${DATABASE_URL}' -p 8000:8000 page_analyzer:latest
--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -5,11 +5,8 @@ def parser_body(body):
     pars_body = BeautifulSoup(body, 'html.parser')
     title = pars_body.title.string
     h1 = pars_body.h1.string if pars_body.h1 is not None else None
-    description = ''
-    if pars_body.meta is not None:
-        metas = pars_body.find_all('meta')
-        for i in metas:
-            if i.get('name') == 'description':
-                description = i['content']
+    description = pars_body.find('meta', {'name': 'description'}). \
+        get('content') \
+        if pars_body.find('meta', {'name': 'description'}) is not None else ''
     pars_data = {'title': title, 'h1': h1, 'description': description}
     return pars_data
