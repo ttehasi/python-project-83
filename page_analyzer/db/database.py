2fdb9dc0146c9be0d0a9c1ebf62e8baec1026098
ttehasi <tatiana.chudowa@my.com>
2025-05-19 22:10:37 +0300

    add replace in database_url

--- page_analyzer/db/database.py
+++ page_analyzer/db/database.py
@@ -2,13 +2,13 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.dialects import registry
+from sqlalchemy.orm import sessionmaker
 
 from page_analyzer.db.models import Base
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
+DATABASE_URL = os.getenv('DATABASE_URL').replace('postgres://', 'postgresql://')
 registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
 engine = create_engine(DATABASE_URL)
 session_factory = sessionmaker(bind=engine)
