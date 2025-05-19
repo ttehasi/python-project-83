d3aa321211d6010507b0ae3c846e2486b95dfad9
ttehasi <tatiana.chudowa@my.com>
2025-05-19 13:53:59 +0300

    Revert "add README.md"
    This reverts commit 2c5f44e9bbf4c9cbb935edd23cb2e470b4087f86.

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -5,8 +5,11 @@ def parser_body(body):
     pars_body = BeautifulSoup(body, 'html.parser')
     title = pars_body.title.string
     h1 = pars_body.h1.string if pars_body.h1 is not None else None
-    description = pars_body.find('meta', {'name': 'description'}). \
-        get('content') \
-        if pars_body.find('meta', {'name': 'description'}) is not None else ''
+    description = ''
+    if pars_body.meta is not None:
+        metas = pars_body.find_all('meta')
+        for i in metas:
+            if i.get('name') == 'description':
+                description = i['content']
     pars_data = {'title': title, 'h1': h1, 'description': description}
     return pars_data
