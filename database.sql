808ba4d05eddf535a277a6c7bcd0a6cf32fa595b
ttehasi <tatiana.chudowa@my.com>
2025-05-19 22:19:35 +0300

    add database.sql

---
+++ database.sql
@@ -0,0 +1,14 @@
+ CREATE TABLE IF NOT EXISTS urls (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+ );
+  CREATE TABLE IF NOT EXISTS url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INT REFERENCES urls (id) NOT NULL,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255)  NOT NULL,
+    description VARCHAR(255)  NOT NULL,
+    created_at DATE DEFAULT CURRENT_TIMESTAMP
+ );
