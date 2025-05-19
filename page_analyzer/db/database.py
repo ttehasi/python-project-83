00ee9b0723cdfe47caec21726bb788be7411acaf
ttehasi <tatiana.chudowa@my.com>
2025-05-19 21:50:20 +0300

    add dialect snowflake in sqlalchemy

--- page_analyzer/db/database.py
+++ page_analyzer/db/database.py
@@ -3,11 +3,13 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects import registry
 
 from page_analyzer.db.models import Base
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
+registry.register('snowflake', 'snowflake.sqlalchemy', 'dialect')
 engine = create_engine(DATABASE_URL)
 session_factory = sessionmaker(bind=engine)
 Base.metadata.create_all(bind=engine)
